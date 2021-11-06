fb76c1f1579a7392a59d74681c63fbdb908f4892
Argus-Khan <m.a.khan.oea@gmail.com>
2021-11-06 15:40:45 +0500

    Test Commit

---
+++ popup-btn.js
@@ -0,0 +1,13 @@
+var popUp   = document.getElementById('popup')
+var overLay = document.getElementById('overlay')
+
+
+function showPopup(){
+    popUp.style.visibility = "visible"
+    overLay.style.visibility = "visible"
+}
+
+function hidePopup(){
+    popUp.style.visibility = "hidden"
+    overLay.style.visibility = "hidden"
+}
