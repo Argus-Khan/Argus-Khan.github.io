9fee2e25b7c021a12a57e305bd6e8ebbf62a8d6b
Argus Khan <m.a.khan.oea@gmail.com>
2025-04-14 06:41:49 +0200

    Icons and links updated

--- assets/index-DeFPRz_X.js
+++ assets/index-DeFPRz_X.js
@@ -1,10 +1,9 @@
-(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))d(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const r of t.addedNodes)r.tagName==="LINK"&&r.rel==="modulepreload"&&d(r)}).observe(document,{childList:!0,subtree:!0});function m(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function d(e){if(e.ep)return;e.ep=!0;const t=m(e);fetch(e.href,t)}})();document.querySelector("#navB").innerHTML=`
+(function(){const n=document.createElement("link").relList;if(n&&n.supports&&n.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))d(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const r of t.addedNodes)r.tagName==="LINK"&&r.rel==="modulepreload"&&d(r)}).observe(document,{childList:!0,subtree:!0});function m(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function d(e){if(e.ep)return;e.ep=!0;const t=m(e);fetch(e.href,t)}})();document.querySelector("#navB").innerHTML=`
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div class="flex justify-between h-20">
                 <div class="flex items-center">
-                    <a href="#" class="flex-shrink-0 flex items-center z-10">
-                        <img class="h-10 w-auto" src="resources/1024px-Video-Game-Controller-Icon-IDV-green.svg.png" alt="Logo">
-                        <!-- <span class="ml-2 text-xl font-bold text-white">Packits</span> -->
+                    <a href="https://morphykun0vvv0.itch.io/" class="flex-shrink-0 flex items-center z-10">
+                        <img class="h-10 p-1 w-auto rounded-full bg-[#cba6f7]" src="/resources/EmbosedHeart_v.png" alt="Logo">
                     </a>
                 </div>
                 <div class="flex items-center">
@@ -24,7 +23,7 @@
                         <a href="#" class="nerd-font text-indigo-600 py-1 px-3 rounded-full text-3xl">󰞦</a>
                     </div>
                     <div class="flex items-center md:hidden" x-data="{navExpand: false}">
-                        <button id="navToggle" type="button" class="z-10 inline-flex items-center justify-center p-2 rounded-full text-[#b4befe] hover:text-[#b4befe] hover:bg-gray-700 shadow-lg shadow-[#89dceb]/50" aria-expanded="false" x-on:click="navExpand = ! navExpand">
+                        <button id="navToggle" type="button" class="z-10 inline-flex items-center justify-center p-2 rounded-full text-[#b4befe] hover:text-[#b4befe] hover:bg-gray-700 shadow-lg shadow-[#cba6f7]/50" x-on:click="navExpand = ! navExpand">
                             <svg class="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
                             </svg>
@@ -32,10 +31,7 @@
                 
                 <div x-show="navExpand" id="mobile-menu">
 
-            <div id="dropdownNav" class="px-5 pt-25 pb-10 space-y-1 absolute top-0 left-0 w-screen border-b-2 border-[#74c7ec] rounded-b-2xl dropdownNav shadow-lg shadow-[#89dceb]/50 text-center z-5">
-                <a href="#" class="text-gray-300 hover:text-white hover:bg-[#45475a] block px-3 py-2 rounded-md text-base font-medium">
-                    About
-                </a>
+            <div id="dropdownNav" class="px-5 pt-25 pb-10 space-y-1 absolute top-0 left-0 w-screen border-b-2 border-[#cba6f7] rounded-b-2xl dropdownNav shadow-lg shadow-[#cba6f7]/50 text-center z-5"> 
                 <a href="#" class="text-gray-300 hover:text-white hover:bg-[#45475a] block px-3 py-2 rounded-md text-base font-medium">
                     Projects
                 </a>
@@ -54,7 +50,7 @@
       </nav>
 `;document.querySelector("#intro").innerHTML=`
     <div class="text-left nerd-font pt-25 px-10 xl:px-50 sm:px-10 md:px-25 w-screen fixed sm:top-10 md:top-10 lg:top-15 xl:top-60" x-data="{ mesg: false }">
-      <h1 class="text-4xl text-[#f5c2e7] ">Hey there! I'm <span class="bg-clip-text text-transparent bg-gradient-to-r from-purple-500 to-pink-500 animate-wiggle">Argus</span>.</h1>
+      <h1 class="text-4xl text-[#f5c2e7] ">Heya! I'm <span class="bg-clip-text text-transparent bg-gradient-to-r from-purple-500 to-pink-500 animate-wiggle">Argus</span>.</h1>
       <p class="text-[#cba6f7] mt-5">I am a Computer Science student (final year) with brief professional experience as a Junior Front-End Developer, and an occasional Indie Game Developer.
 
         <span x-show="mesg" x-transition> Currently I'm pursuing my DevOps certifications as well as Full-Stack Dev Certifications.
@@ -68,4 +64,4 @@ a guitar.</span><br>
 </p> 
       <h1 id="typewriter" class="text-[#a6e3a1] mt-5 text-3xl fixed bottom-20 sm:bottom-10 md:bottom-20 lg:bottom-25 xl:bottom-90 animate-blinking" ></h1>
 </div>
-`;const l=a=>new Promise(s=>setTimeout(s,a)),p=" +    +    = 󱅾 Coding...";let o=0,n="",i=!1;async function c(){n=p,i?(document.querySelector("#typewriter").textContent=n.substring(0,o-1),o--,o==0&&(await l(1e3),i=!1)):(document.querySelector("#typewriter").textContent=n.substring(0,o+1),o++,o==n.length&&(await l(3e3),i=!0)),setTimeout(c,30)}c();
+`;const l=a=>new Promise(n=>setTimeout(n,a)),p=" +    +    = 󱅾 Coding...";let o=0,s="",i=!1;async function c(){s=p,i?(document.querySelector("#typewriter").textContent=s.substring(0,o-1),o--,o==0&&(await l(1e3),i=!1)):(document.querySelector("#typewriter").textContent=s.substring(0,o+1),o++,o==s.length&&(await l(3e3),i=!0)),setTimeout(c,30)}c();
